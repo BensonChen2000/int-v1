6213145b4c0018ff362b48e3749137ac7aa4a1ff
Benson <zc3chen@uwaterloo.ca>
2023-04-26 17:36:46 -0400

    to send and recieve packets continuously

---
+++ sendCont.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+import argparse
+import sys
+import socket
+import random
+import struct
+
+
+from scapy.all import sendp, send, get_if_list, get_if_hwaddr
+from scapy.all import Packet
+from scapy.all import Ether, IP, UDP, TCP
+
+
+def get_if():
+    ifs = get_if_list()
+    iface = None  # "h1-eth0"
+    for i in get_if_list():
+        if "eth0" in i:
+            iface = i
+            break
+    if not iface:
+        print("Cannot find eth0 interface")
+        exit(1)
+    return iface
+
+
+def main():
+
+    if len(sys.argv) < 3:
+        print('pass 2 arguments: <destination> "<message>"')
+        exit(1)
+
+    addr = socket.gethostbyname(sys.argv[1])
+    iface = get_if()
+
+    print(f"sending on interface {iface} to {str(addr)}")
+
+
+    pkt = Ether(src=get_if_hwaddr(iface), dst="08:00:00:00:02:22") / \
+        IP(dst=addr) / UDP(sport=8001, dport=8002) / sys.argv[2]
+    # pkt.show2()
+    sendp(pkt, iface=iface, loop=True, verbose=True)
+
+
+if __name__ == '__main__':
+    main()
